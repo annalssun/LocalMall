5fde5196138c81473f47443b65d9a502983e00c0
WIN7-1706211330\Administrator <864307818@qq.com>
2017-11-08 11:14:39 +0800

    项目初始化第一次提交

---
+++ android/app/src/main/java/sun/annals/com/localmall/MainActivity.java
@@ -0,0 +1,11 @@
+package sun.annals.com.localmall;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+   @Override
+    protected String getMainComponentName() {
+        return "localmall";
+    }
+}
