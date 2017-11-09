021b5f58a171464e562e59f75240074556178847
WIN7-1706211330\Administrator <864307818@qq.com>
2017-11-09 11:46:28 +0800

    Redux学习第二天

---
+++ APP/pages/centerContent/login.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import React,{Component} from 'react';
+
+import {
+
+} from 'react-native';
+
+class Login extends Component{
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+        return(
+            
+        );
+    }
+}
+
--- APP/reducers/index.js
+++ APP/reducers/index.js
@@ -1,7 +1,8 @@
 'use strict';
 import {combineReducers} from 'redux';
+import login from './LoginReducer';
 
 const rootReducer = combineReducers({
-
+    login
 })
 export default rootReducer;
